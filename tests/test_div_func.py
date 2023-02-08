3efe9c209ae777aad93abaaff3f0fd2206c26d9b
alexmudra <ko9#ba9$>
2023-02-08 14:01:27 +0200

    add 6 positive, negative test

--- tests/test_div_func.py
+++ tests/test_div_func.py
@@ -1,5 +1,20 @@
 #https://www.youtube.com/watch?v=1HtEPEn4-LY
 from utils.test_div_func import division
+import pytest
 
-def test_div_positive_with_2_int():
-    assert division(10,2) == 5
+@pytest.mark.parametrize('f_st_value, second_value, expected_result', [(10, 5, 2),
+                                                                       (20, 5, 4),
+                                                                       (100, 20, 5),
+                                                                       (-10, 5, -2)])
+def test_div_positive_with_2_int(f_st_value, second_value, expected_result ):
+    assert division(f_st_value, second_value) == expected_result
+
+
+# @pytest.mark.parametrize('f_st_value, second_value', [(10, 0)])
+def test_ZeroDivisionError():
+    with pytest.raises(ZeroDivisionError):
+        division(20, 0)
+
+def test_TypeError():
+    with pytest.raises(TypeError):
+        division(20, 'str')
--- utils/test_div_func.py
+++ utils/test_div_func.py
@@ -1,5 +1,4 @@
-def division(frst_num:int, scnd_num:int):
-    print(f'Result ', frst_num/scnd_num)
+def division(frst_num, scnd_num):
     return  frst_num / scnd_num
 
 
