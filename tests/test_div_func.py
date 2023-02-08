a88ea966cb217618c3477cac5d940018d29deed8
alexmudra <ko9#ba9$>
2023-02-08 13:06:29 +0200

    add division positive test

---
+++ tests/test_div_func.py
@@ -0,0 +1,5 @@
+#https://www.youtube.com/watch?v=1HtEPEn4-LY
+from utils.test_div_func import division
+
+def test_div_positive_with_2_int():
+    assert division(10,2) == 5
---
+++ utils/test_div_func.py
@@ -0,0 +1,5 @@
+def division(frst_num:int, scnd_num:int):
+    print(f'Result ', frst_num/scnd_num)
+    return  frst_num / scnd_num
+
+
