5765f9bc09f43b91f8204e035d34727660f24c7b
alexmudra <ko9#ba9$>
2023-02-10 12:41:38 +0200

    add create test data func as example

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,11 @@
 import pytest
 
-@pytest.fixture()
+@pytest.fixture(autouse=True)
 def clean_data_before_write():
     with open('tests/test_for_append_data.txt', 'w'):
+        pass #витираємо із файла
+
+@pytest.fixture(autouse=True)
+def clean_data_in_copiedtxt_write():
+    with open('tests/info_file_copied.txt', 'w'):
         pass #витираємо із файла
--- tests/test_file_workers.py
+++ tests/test_file_workers.py
@@ -1,18 +1,31 @@
 #https://www.youtube.com/watch?v=MIHXRF6YMN4&t=614s
 from my_funcs.file_worker import read_form_file
 
+def create_test_data(test_data): #запис тестових даних в файл, test_data буде як параметр у кожному файлі
+    with open('tests/test_for_append_data.txt', 'a') as f_ob:
+        f_ob.writelines(test_data)
+
+
+def create_test_data_copied(test_data): #запис тестових даних в файл, test_data буде як параметр у кожному файлі
+    with open('tests/info_file_copied.txt', 'a') as f_ob:
+        f_ob.writelines(test_data)
+
+
 def test_read_fom_file():
     expect_res = ['one\n', 'twoo\n', 'three\n', 'four\n', 'fife']
+    create_test_data_copied(expect_res)
     assert  expect_res == read_form_file('tests/info_file_copied.txt')
+
 
 def test_read_fom_file_2():
     expect_res = ['one\n', 'twoo\n', 'three\n', 'four\n', 'fife']
+    create_test_data_copied(expect_res)
     assert  expect_res == read_form_file('tests/info_file_copied.txt')
 
+
 def test_read_fom_file_append_data():
-    with open('tests/test_for_append_data.txt', 'w'):
-        pass #витремо наді в файлі. Цей код можна перенести в якості фікстури
-    test_data = ['one\n', 'twoo\n', 'three\n', 'four\n', 'fife'] #підготуємо тестові дані
-    with open('tests/test_for_append_data.txt', 'a') as f_ob:
-        f_ob.writelines(test_data)
+    # with open('tests/test_for_append_data.txt', 'w'):
+    #     pass #витремо наді в файлі. Цей код можна перенести в якості фікстури
+    test_data = ['one\n', 'twoo\n', 'three\n', 'four\n', 'fife\n', 'six'] #підготуємо тестові дані
+    create_test_data(test_data)
     assert  test_data == read_form_file('tests/test_for_append_data.txt')
