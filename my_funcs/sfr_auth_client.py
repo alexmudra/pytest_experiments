d9e15fca315cd5e620f1b040bdf47d70513935ef
alexmudra <ko9#ba9$>
2023-02-13 11:37:55 +0200

    change User-Agent from python-requests/2.28.2 to User-Agent: PostmanRuntime/7.30.1

--- my_funcs/sfr_auth_client.py
+++ my_funcs/sfr_auth_client.py
@@ -1,6 +1,8 @@
 import requests
 import json
 import datetime
+import logging
+from http.client import HTTPConnection
 
 #http://marketplace.sphera.byustudio.in.ua/marketplace/users/signin
 
@@ -17,18 +19,45 @@ def get_auth_login_status():
     headers = {"Content-Type": "application/json; Charset=UTF-8"}
     resp = requests.get(base_host + '/users/signin', headers=headers)
     resp.raise_for_status()  # raises exception when not a 2xx response
-    if resp.status_code != 204:
+    if resp.status_code != [200, 201, 202, 204]:
         return resp.json()
-    # json_data = resp.status_code
     print(resp)
     # return json_data
 
 
 # if __name__ == "__main__":
 
-get_auth_login_status( )
+# get_auth_login_status( )
+
+json_file_path = 'http://marketplace.sphera.byustudio.in.ua/marketplace/users/signin'
+headers = {"Content-Type": "application/json; Charset=UTF-8"}
+
+def get_auth_viastoreto_file():
+    # r = requests.get('http://marketplace.sphera.byustudio.in.ua/marketplace/users/signin', headers={'Content-Type': 'application/json; charset=UTF-8'})
+    # print (requests.get('http://marketplace.sphera.byustudio.in.ua/marketplace/users/signin', headers={'Content-Type': 'application/json; charset=UTF-8'}))
+    HTTPConnection.debuglevel = 1
+    headers = {'Content-type': 'application/json', 'Accept': 'text/plain',  'Charset':'UTF-8', 'User-Agent': 'PostmanRuntime/7.30.1'}
+    r = requests.get('http://marketplace.sphera.byustudio.in.ua/marketplace/users/signin', headers=headers)
+
+    print("Це урл на який відправляється запит", r.url)
+    print("type респонса:", type(r))
+    print("Статус респонса, str:", r) #<Response [403]>
+    # print("Це тип відповіді:",type(r.content))
+    # print("Це тип decode:",(r.content).decode("utf-8"))
+    # print("Це відображення контенту:",r.content)
+    # print("Це відображається статус відповіді, int:", r.status_code)
+    # print("Відображення респонсу: ", r.text)
+    # print(a =json.loads(r.text))
+    # print("Це відображення headers респонса: ", r.headers())
+    print("Це декодований респонс в json формат: ", r.json())
+
+    # data = r.text
+    # # try:
+    # data_json = json.loads(data)
+    # print(data_json)
+    # # except json.JSONDecodeError:
+    # #     print("Empty response")
 
 
-
-    # s = SfraAuthClient('http://marketplace.sphera.byustudio.in.ua/marketplace')
-    # print(s.get_auth_login())
+if __name__ == "__main__":
+    get_auth_viastoreto_file()
