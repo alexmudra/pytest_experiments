912b75320973ade2a50c0fabeff34e50e5b4b9ff
alexmudra <ko9#ba9$>
2023-02-15 18:04:27 +0200

    Add examples mysql requests

--- tests/test_auth_sfra.py
+++ tests/test_auth_sfra.py
@@ -2,6 +2,16 @@ import pytest
 from my_funcs.sfr_auth_client import get_auth_json_response_body
 from my_funcs.sfr_auth_client import get_auth_login_status
 
-def test_check_403_login_status():
-    exp_auth_login_status = get_auth_login_status()
-    assert exp_auth_login_status == 403
+@pytest.mark.parametrize('exp_res, act_res', [(get_auth_login_status(), 403)])
+def test_check_403_login_status(exp_res, act_res):
+    # exp_auth_login_status = get_auth_login_status()
+    # assert exp_auth_login_status == 403
+    assert exp_res == act_res
+
+@pytest.mark.parametrize('exp_res, act_res', [(get_auth_json_response_body()['authorization']['login'], 'guest')
+                                              ])
+def test_login_is_guest(exp_res, act_res):
+    # exp_res = get_auth_json_response_body()['authorization']['login']
+    assert exp_res == act_res, \
+    f"Incorrect type of authorization.login has been returned: {get_auth_json_response_body()['authorization']['login']}"
+
